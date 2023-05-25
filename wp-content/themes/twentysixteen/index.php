d0220cda4f6ec9ecae9723391efdb3c54ffeb81f
Ramón <ramon.mosquera@gmail.com>
2023-05-25 18:55:04 +0200

    Newsletter 202306 + recongnitions

--- wp-content/themes/twentysixteen/index.php
+++ wp-content/themes/twentysixteen/index.php
@@ -70,10 +70,41 @@ get_header();
 
                     endif;
                     ?>
+                </div>
                 <div class="col-md-2 col-sm-12 col-xs-12">
                     <h3 style="text-align: center;margin-bottom: 20px;">Newsletters</h5>
                     <div class="row">
                         <div class="col-md-12 col-sm-6 col-xs-12 newsletters">
+                            <div class="newsletter">
+                                <a href="/newsletter/2023_junio.html" title="Junio 2023">
+                                    <img src="/newsletter/img/202306/screenshot.png" alt="Junio 2023">
+                                    <h5>Junio 2023</h5>
+                                </a>
+                            </div>
+                            <div class="newsletter">
+                                <a href="/newsletter/2023_marzo.html" title="Marzo 2023">
+                                    <img src="/newsletter/img/202303/screenshot.png" alt="Marzo 2023">
+                                    <h5>Marzo 2023</h5>
+                                </a>
+                            </div>
+                            <div class="newsletter">
+                                <a href="/newsletter/2022_diciembre.html" title="Diciembre 2022">
+                                    <img src="/newsletter/img/202212/screenshot.png" alt="Diciembre 2022">
+                                    <h5>Diciembre 2022</h5>
+                                </a>
+                            </div>
+                            <div class="newsletter">
+                                <a href="/newsletter/2022_septiembre.html" title="Septiembre 2022">
+                                    <img src="/newsletter/img/202209/screenshot.png" alt="Septiembre 2022">
+                                    <h5>Septiembre 2022</h5>
+                                </a>
+                            </div>
+                            <div class="newsletter">
+                                <a href="/newsletter/2022_junio.html" title="Junio 2022">
+                                    <img src="/newsletter/img/202206/screenshot.png" alt="Junio 2022">
+                                    <h5>Junio 2022</h5>
+                                </a>
+                            </div>
                             <div class="newsletter">
                                 <a href="/newsletter/2022_marzo.html" title="Marzo 2022">
                                     <img src="/newsletter/img/202203/screenshot.png" alt="Marzo 2022">
