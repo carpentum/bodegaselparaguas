4bcb4351fb8b996873757d9adbacd1a0abb3b1a2
Ramón <ramon.mosquera@gmail.com>
2025-11-16 21:38:27 +0100

    202512 newsletter

--- wp-content/themes/twentysixteen/index.php
+++ wp-content/themes/twentysixteen/index.php
@@ -72,6 +72,12 @@ get_header();
                         <div class="row">
                             <div class="col-md-12 col-md-offset-0 col-sm-6 col-sm-offset-3 col-xs-12 newsletters">
                                 <div class="newsletter">
+                                    <a href="/newsletter/2025_diciembre.html" title="Diciembre 2025">
+                                        <img src="/newsletter/img/202512/screenshot.png" alt="Diciembre 2025">
+                                        <h5>Diciembre 2025</h5>
+                                    </a>
+                                </div>
+                                <div class="newsletter">
                                     <a href="/newsletter/2025_septiembre.html" title="Septiembre 2025">
                                         <img src="/newsletter/img/202509/screenshot.png" alt="Septiembre 2025">
                                         <h5>Septiembre 2025</h5>
